603a91030f95c28e1ad76e0ade5ca25c36bebf59
Kitan Garcia <kitangar@gmail.com>
2023-04-16 14:11:40 -0700

    find occurrence of regex as word(s) instead of substring

--- static/app.js
+++ static/app.js
@@ -13,17 +13,29 @@ const Controller = {
   updateTable: (results, query) => {
     const table = document.getElementById("table-body");
     const rows = [];
-    const queryRegEx = new RegExp(query.value, "ig");
 
+    // Regex to find occurence of string as word(s) as opposed to as substring
+    const queryRegEx = new RegExp("\\b" + query.value + "\\b", "ig");
+
+    if (results.length === 0) {
+      alert("String not present in Shakespeare's works. Please try another.");
+      return;
+    }
+
+    // Re-index to filter only count matches of entire word as opposed to substring
+    let resultCount = 0;
     for (let i = 0; i < results.length; i++) {
       if (i === 0) {
         rows.push("<tr id='first-row'><th>Result</th><th>Text</th></tr>");
       }
       results[i] = highlightQuery(results[i], queryRegEx);
 
-      rows.push(
-        `<tr><td class="result-number">${i + 1}</td><td>${results[i]}</td></tr>`
-      );
+      if (results[i].length > 0) {
+        resultCount++;
+        rows.push(
+          `<tr><td class="result-number">${resultCount}</td><td>${results[i]}</td></tr>`
+        );
+      }
     }
     table.innerHTML = rows.join("");
   },
@@ -33,8 +45,11 @@ const Controller = {
 const highlightQuery = (passage, queryRegEx) => {
   let matches = passage.match(queryRegEx);
   let matchedSubstrings = passage.split(queryRegEx);
-  passage = "";
+  if (!matches || matches.length === 0) {
+    return "";
+  }
 
+  passage = "";
   for (let j = 0; j < matches.length; j++) {
     matches[j] = `<span class="highlight">${matches[j]}</span>`;
     passage += matchedSubstrings[j] + matches[j];
