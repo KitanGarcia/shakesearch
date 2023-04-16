b1775e9679a3e25210a3fd5f572692b89b004481
Kitan Garcia <kitangar@gmail.com>
2023-04-15 22:12:01 -0700

    highlight query in search results

--- static/app.js
+++ static/app.js
@@ -5,18 +5,36 @@ const Controller = {
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateTable(results, query);
       });
     });
   },
 
-  updateTable: (results) => {
+  updateTable: (results, query) => {
     const table = document.getElementById("table-body");
     const rows = [];
+    const queryRegEx = new RegExp(query.value, "ig");
+
+    let match = "";
+    let matchArray = [];
+
     for (let i = 0; i < results.length; i++) {
       if (i === 0) {
         rows.push("<tr id='first-row'><th>Result</th><th>Text</th></tr>");
       }
+
+      // Add spans to highlight regex matches
+      match = results[i].match(queryRegEx);
+      matchArray = results[i].split(queryRegEx);
+      results[i] = "";
+
+      for (let j = 0; j < match.length; j++) {
+        match[j] = `<span class="highlight">${match[j]}</span>`;
+        results[i] += matchArray[j] + match[j];
+      }
+
+      results[i] += matchArray[matchArray.length - 1];
+
       rows.push(
         `<tr><td class="result-number">${i + 1}</td><td>${results[i]}</td></tr>`
       );
