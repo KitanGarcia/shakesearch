43e83e162c8b878d1ccb05b40fb0e8f10d194e30
Kitan Garcia <kitangar@gmail.com>
2023-04-16 12:38:37 -0700

    move highlight logic to function

--- static/app.js
+++ static/app.js
@@ -15,25 +15,11 @@ const Controller = {
     const rows = [];
     const queryRegEx = new RegExp(query.value, "ig");
 
-    let match = "";
-    let matchArray = [];
-
     for (let i = 0; i < results.length; i++) {
       if (i === 0) {
         rows.push("<tr id='first-row'><th>Result</th><th>Text</th></tr>");
       }
-
-      // Add spans to highlight regex matches
-      match = results[i].match(queryRegEx);
-      matchArray = results[i].split(queryRegEx);
-      results[i] = "";
-
-      for (let j = 0; j < match.length; j++) {
-        match[j] = `<span class="highlight">${match[j]}</span>`;
-        results[i] += matchArray[j] + match[j];
-      }
-
-      results[i] += matchArray[matchArray.length - 1];
+      results[i] = highlightQuery(results[i], queryRegEx);
 
       rows.push(
         `<tr><td class="result-number">${i + 1}</td><td>${results[i]}</td></tr>`
@@ -41,6 +27,21 @@ const Controller = {
     }
     table.innerHTML = rows.join("");
   },
+};
+
+// Adds spans to highlight regex matches
+const highlightQuery = (passage, queryRegEx) => {
+  let matches = passage.match(queryRegEx);
+  let matchedSubstrings = passage.split(queryRegEx);
+  passage = "";
+
+  for (let j = 0; j < matches.length; j++) {
+    matches[j] = `<span class="highlight">${matches[j]}</span>`;
+    passage += matchedSubstrings[j] + matches[j];
+  }
+
+  passage += matchedSubstrings[matchedSubstrings.length - 1];
+  return passage;
 };
 
 const form = document.getElementById("form");
